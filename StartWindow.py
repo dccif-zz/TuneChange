69d9b9d21a30c1b5be967d9aab2b13fdc5e5bf55
dccif <heyujieboy@163.com>
2017-11-26 21:27:05 +0800

    Optimize the Regex
    Former-commit-id: 12b655b852b10520b202325eb5de31aa4a7ff1c8

--- StartWindow.py
+++ StartWindow.py
@@ -14,6 +14,7 @@ InputText = ""
 MidList = ""
 ChangeText = ""
 
+
 class MainWindow(GUI.TuneChangeGui):
     def init_main_window(self):
         return
@@ -32,25 +33,24 @@ class MainWindow(GUI.TuneChangeGui):
         MidList = re.sub(",|，|\s+", " ", InputText)
         MidList = re.split(",|，|\s+", MidList)
         Scale[0] = Scale[1] - Scale[2]
-# Use Regex To Check Input
-# if Check Pass start Change
-        if(self.CheckInput(MidList)):
-            NumList = list(map(lambda x: KeyMap[x], MidList))
-            ChangeList = list(map(lambda x: x + Scale[0], NumList))
-            ChangeList = list(map(lambda x: NumKey[x], ChangeList))
+        # Use Regex To Check Input
+        # if Check Pass start Change
+        if self.CheckInput(MidList):
+            num_list = list(map(lambda x: KeyMap[x], MidList))
+            change_list = list(map(lambda x: x + Scale[0], num_list))
+            change_list = list(map(lambda x: NumKey[x], change_list))
 
         global ChangeText
-        ChangeText = ",".join(ChangeList)
+        ChangeText = ",".join(change_list)
         self.After.SetValue(ChangeText)
 
     def CheckInput(self, inlist):
-        inlength = MidList.__len__();
-        # rmatch = re.compile(r'(?:#|0|#0)?(?:[1-7](?!\d)|([1-7])\1\b)')
-        rmatch = re.compile(r'#?(?:0?[1-7](?!\d)|\b([1-7])\1\b)')
-        if (inlist[-1] == ''):
+        inlength = MidList.__len__()
+        rmatch = re.compile(r'#?(?:0?[1-7]\b|([1-7])\1\b)')
+        if '' == inlist[-1]:
             inlength -= 1
-        CheckList = list(filter(rmatch.match,inlist))
-        if(CheckList.__len__() == inlength):
+        check_list = list(filter(rmatch.match, inlist))
+        if check_list.__len__() == inlength:
             return True
         self.After.SetValue("很抱歉，输入格式错误")
         self.Before.Clear()
